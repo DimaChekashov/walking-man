9720e66f912a3d4b193e4379bc1bfa020ac97121
DimaChekashov <foxdima99@gmail.com>
2018-10-01 10:41:04 +0300

    some change

--- js/action.js
+++ js/action.js
@@ -1,28 +1,36 @@
-Man.prototype.draw = function(sprite) {
+function clearSprite(){
   ctx.clearRect(0, 0, scene.width, scene.height);
+}
+
+Man.prototype.draw = function(sprite) {
+  clearSprite();
   ctx.drawImage(sprite, 0, 0, 80, 110, xPos, yPos, 80, 110);
 }
 
 Man.prototype.walkRight = function(sprite) {
-  ctx.clearRect(0, 0, scene.width, scene.height);
+  clearSprite();
   xPos = (xPos === 600 ? 0 : xPos + 10);
   ctx.drawImage(sprite, 0, 110, 80, 110, xPos, yPos, 80, 110);
+  function draw(x) {
+    console.log(x);
+  }
+  draw(xPos);
 }
 
 Man.prototype.walkLeft = function(sprite) {
-  ctx.clearRect(0, 0, scene.width, scene.height);
+  clearSprite();
   xPos = (xPos === 0 ? 520 : xPos - 10);
   ctx.drawImage(sprite, 0, 110, 80, 110, xPos, yPos, 80, 110);
 }
 
 Man.prototype.jump = function(sprite) {
-  ctx.clearRect(0, 0, scene.width, scene.height);
+  clearSprite();
   x = (x === 240 ? 0 : x + 80);
   ctx.drawImage(sprite, x, 0, 80, 110, xPos, yPos, 80, 110);
 }
 
 Man.prototype.seat = function(sprite) {
-  ctx.clearRect(0, 0, scene.width, scene.height);
+  clearSprite();
   ctx.drawImage(sprite, 240, 0, 80, 110, xPos, yPos, 80, 110);
 }
 
